d1be5972ee6975334338255790d4217c1443a34e
Miriam <m.f.sterl@uu.nl>
2021-01-08 16:03:03 +0100

    Update pathes.py
    Paths are now imported from start.py in predictions folder instead of private.py

--- ninolearn/pathes.py
+++ ninolearn/pathes.py
@@ -7,16 +7,16 @@ you public repository
 from os.path import join
 
 try:
-    from ninolearn.private import datadir
+    from predictions.start import datadir # instead of: ninolearn.private
 except ImportError:
     raise ImportError("Cannot import name 'datadir'. Specifiy the path to your data directory using the name 'datadir' in the  ninolearn.private module which you may not commit to you public repository")
 
 try:
-    from ninolearn.private import basedir
+    from predictions.start import basedir
 except ImportError:
     raise ImportError("Cannot import name 'basedir'. Specifiy the path to the root directory of ninolearn")
 
 rawdir = join(datadir, 'raw')
 processeddir = join(datadir, 'processed')
 modeldir = join(datadir, 'model')
-ed_model_dir = join(datadir, 'ED_model')
+ed_model_dir = join(datadir, 'ED_model') # CHECK IF NECESSARY?
