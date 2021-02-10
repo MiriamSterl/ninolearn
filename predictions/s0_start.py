7406b3f4cff1cc7e939d427e7839f58732474a21
Miriam <m.f.sterl@uu.nl>
2021-02-10 11:56:39 +0100

    Update month
    Now ask for first desired month of predictions

---
+++ predictions/s0_start.py
@@ -0,0 +1,13 @@
+"""
+Define the base folder as well as the current month and year.
+Do not commit this file to your public repository!
+"""
+
+# directories
+datadir = ""
+plotdir = ""
+basedir = ""
+
+# the first month (1-12) and year for which you want the prediction to start
+month = 
+year = 
